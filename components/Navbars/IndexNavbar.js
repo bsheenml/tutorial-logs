fc164d8fdea2787b490b578c18e85a96ac16cce1
Brendan Sheen <brendan.sheen@gmail.com>
2022-03-30 15:51:26 -0700

    alpha2

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -35,7 +35,7 @@ export default function Navbar(props) {
             id="example-navbar-warning"
           >
             <ul className="flex flex-col lg:flex-row list-none mr-auto">
-              <li className="flex items-space">
+              <li className="flex items-center">
               <button
                   className="bg-blueGray-700 text-white active:bg-blueGray-600 text-xs font-bold uppercase px-2 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
