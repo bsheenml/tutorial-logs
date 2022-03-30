25695986f83cf85a1f0c5d150bf2b54bf9ebf56b
Brendan Sheen <brendan.sheen@gmail.com>
2022-03-30 16:27:08 -0700

    aaaa

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -196,7 +196,7 @@ export default function Sidebar() {
 
             <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
               <li className="items-center">
-                <Link href="#">
+                <Link href="/admin/tutorials-solo">
                   <a
                     href="/admin/tutorials-solo"
                     className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
@@ -213,7 +213,8 @@ export default function Sidebar() {
                     href="#pablo"
                     className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
                   >
-                    <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}
+                    <i className="fas faaaa
+                    -fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}
                     HubSpot in NextJS
                   </a>
                 </Link>
